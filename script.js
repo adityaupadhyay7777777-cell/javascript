55499395c8e257aaca5a38bcc13599b2c254752d
adityaupadhyay7777777-cell <adityaupadhyay7777777@gmail.com>
2025-11-04 01:45:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hii anshika")
+let anshika$="adi";
+console.log(anshika$)
+let obj={adi:"anshika"};
+console.log(obj.adi)
+let age=20;
+let result=age>=18 ? "adult" : "minor"; //ternary operator
+console.log(result)
+console.log(5 != "5")
+console.log(5 !== "5")
+names=["ali","sara"]
+console.log(names.join("-"));
+let name = "Salman";
+let message = `Hello, ${name}!`;
+console.log(message); // "Hello, Salman!"
