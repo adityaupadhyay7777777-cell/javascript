ca9ee949a2b6fb4b707d11bf31dfdcf5f2f748c8
adityaupadhyay7777777-cell <adityaupadhyay7777777@gmail.com>
2025-11-06 02:43:54 +0530

    javascript 2

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-// console.log("hii anshika")
-// let anshika$="adi";
-// console.log(anshika$)
-// let obj={adi:"anshika"};
-// console.log(obj.adi)
-// let age=20;
-// let result=age>=18 ? "adult" : "minor"; //ternary operator
-// console.log(result)
-// console.log(5 != "5")
-// console.log(5 !== "5")
-// names=["ali","sara"]
-// console.log(names.join("-"));
-// let name = "Salman";
-// let message = `Hello, ${name}!`;
-// console.log(message); // "Hello, Salman!"
-
-const button=document.getElementById("btn");
-console.log(button.innerHTML)
+const btn1=document.getElementById("btn");
+console.log(btn1.innerText);
